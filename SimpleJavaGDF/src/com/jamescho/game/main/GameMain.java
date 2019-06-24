20eaa558d62ecd71f45a328aed66179973daef80
Znmangosteen <Znmangosteen@hotmail.com>
2019-06-24 21:59:07 +0800

    finish framework

--- SimpleJavaGDF/src/com/jamescho/game/main/GameMain.java
+++ SimpleJavaGDF/src/com/jamescho/game/main/GameMain.java
@@ -11,12 +11,14 @@ public class GameMain {
     public static void main(String[] args) {
         JFrame frame = new JFrame(GAME_TITLE);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setResizable(false);
 
         sGame = new Game(GAME_WIDTH, GAME_HEIGHT);
         frame.add(sGame);
         frame.pack();
         frame.setVisible(true);
+
+        frame.setIconImage(Resources.iconimage);
     }
 }
