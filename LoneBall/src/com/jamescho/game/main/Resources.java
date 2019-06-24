fca890de59c7afe2fdbbc1ddf80841546f5e23d5
Znmangosteen <Znmangosteen@hotmail.com>
2019-06-25 00:40:11 +0800

    start play state

--- LoneBall/src/com/jamescho/game/main/Resources.java
+++ LoneBall/src/com/jamescho/game/main/Resources.java
@@ -3,6 +3,7 @@ package com.jamescho.game.main;
 import javax.imageio.ImageIO;
 import java.applet.Applet;
 import java.applet.AudioClip;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
@@ -11,10 +12,20 @@ public class Resources {
     public static final String AUDIO_PATH = "/resources/";
     public static final String IMG_PATH = "/resources/";
 
-    public static BufferedImage welcome, iconimage;
+    public static BufferedImage welcome, iconimage, line;
+
+    public static AudioClip hit, bounce;
+
+    public static Color darkBlue, darkRed;
     public static void load() {
         welcome = loadImage("welcome.png");
         iconimage = loadImage("iconimage.png");
+        line = loadImage("line.png");
+        hit = loadSound("hit.wav");
+        bounce = loadSound("bounce.wav");
+
+        darkBlue = new Color(25, 83, 105);
+        darkRed = new Color(105, 13, 13);
     }
 
     public static AudioClip loadSound(String filename) {
--- LoneBall/src/com/jamescho/game/state/PlayState.java
+++ LoneBall/src/com/jamescho/game/state/PlayState.java
@@ -1,16 +1,16 @@
 package com.jamescho.game.state;
 
+import com.jamescho.game.main.GameMain;
 import com.jamescho.game.main.Resources;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
-public class MenuState extends State{
-
+public class PlayState extends State {
     @Override
     public void init() {
-        System.out.println("Entered MenuState");
+
     }
 
     @Override
@@ -20,12 +20,17 @@ public class MenuState extends State{
 
     @Override
     public void render(Graphics graphics) {
-        graphics.drawImage(Resources.welcome, 0, 0, null);
+        graphics.setColor(Resources.darkBlue);
+        graphics.fillRect(0, 0, GameMain.GAME_WIDTH, GameMain.GAME_HEIGHT);
+        graphics.setColor(Resources.darkRed);
+        graphics.fillRect(GameMain.GAME_WIDTH / 2, 0, GameMain.GAME_WIDTH / 2, GameMain.GAME_HEIGHT);
+
+        graphics.drawImage(Resources.line, (GameMain.GAME_WIDTH / 2) -2, 0, null);
     }
 
     @Override
     public void onClick(MouseEvent event) {
-        setCurrentState(new PlayState());
+
     }
 
     @Override
