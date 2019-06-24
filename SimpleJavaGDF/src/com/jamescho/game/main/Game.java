e3debfaa203aea0607b7c209bdaa989e367ba3d5
Znmangosteen <Znmangosteen@hotmail.com>
2019-06-24 15:15:26 +0800

    checkpoint 2

--- SimpleJavaGDF/src/com/jamescho/game/main/Game.java
+++ SimpleJavaGDF/src/com/jamescho/game/main/Game.java
@@ -1,5 +1,8 @@
 package com.jamescho.game.main;
 
+import com.jamescho.game.state.LoadState;
+import com.jamescho.game.state.State;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -12,6 +15,7 @@ public class Game extends JPanel {
     private Thread gameThread;
     private volatile boolean running;
 
+    private volatile State currentState;
 
     public Game(int gameWidth, int gameHeight) {
         this.gameWidth = gameWidth;
@@ -23,4 +27,19 @@ public class Game extends JPanel {
         requestFocus();
 
     }
+
+    public void setCurrentState(State newState) {
+        System.gc();
+        newState.init();
+
+        this.currentState = newState;
+    }
+
+    @Override
+    public void addNotify() {
+        super.addNotify();
+        setCurrentState(new LoadState());
+
+    }
+
 }
---
+++ SimpleJavaGDF/src/com/jamescho/game/state/LoadState.java
@@ -0,0 +1,41 @@
+package com.jamescho.game.state;
+
+import com.jamescho.game.main.Resources;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+public class LoadState extends State {
+
+    @Override
+    public void init() {
+        Resources.load();
+        System.out.println("load successfully");
+    }
+
+    @Override
+    public void update() {
+        setCurrentState(new MenuState());
+    }
+
+    @Override
+    public void render(Graphics graphics) {
+
+    }
+
+    @Override
+    public void onClick(MouseEvent event) {
+
+    }
+
+    @Override
+    public void onKeyPress(KeyEvent event) {
+
+    }
+
+    @Override
+    public void OnKeyRelease(KeyEvent event) {
+
+    }
+}
